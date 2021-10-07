73b60d351f574afaa3ae10d529a3a98de2550ac1
Najiya Nazrin P N <71110070+najiyanazrinpn@users.noreply.github.com>
2021-10-07 17:13:33 +0530

    Update ex5pgm3.sh

--- ex5pgm3.sh
+++ ex5pgm3.sh
@@ -1,8 +1,8 @@
 #Write a shell script that accepts two integers as its arguments and computes the value of first number raised to the power of the second number.
 
 #! /bin/bash
-read -p "Enter integer1 : " n1
-read -p "Enter integer2 : " n2
+n1=$1
+n2=$2
 result=1
 echo -n "$n1^$n2 : "
 while [ $n2 -ne 0 ]
