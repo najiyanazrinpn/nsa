01dd2eff6525641f8f5ac20f03f967b966d1cea8
Najiya Nazrin P N <71110070+najiyanazrinpn@users.noreply.github.com>
2021-10-07 17:16:42 +0530

    Update ex6pgm2.sh

--- ex6pgm2.sh
+++ ex6pgm2.sh
@@ -1,3 +1,8 @@
+#2) Write shell script to show various system configurations like
+#   • your OS and version, release number, kernel version
+#   •all available shells
+#   • File system (Mounted)
+
 #! /bin/bash
 echo "-----Operating System-----"
 uname -o
