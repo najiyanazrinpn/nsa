652da70b94cd58d7d68d470f5740ae400ce4e425
Najiya Nazrin P N <71110070+najiyanazrinpn@users.noreply.github.com>
2021-10-07 17:11:39 +0530

    Update ex5pgm2.sh

--- ex5pgm2.sh
+++ ex5pgm2.sh
@@ -1,3 +1,7 @@
+#Write a shell script that computes the gross salary of a employee according to the following rules :
+#i) if basic salary is < 1500 then HRA =10% of the basic and DA =90% of the basic.
+#ii) If basic salary is >=1500 then HRA =Rs500 and DA=98% of the basic.
+
 #! /bin/bash
 read -p "Enter a:: " basic
 if [ $basic -lt 1500 ]
